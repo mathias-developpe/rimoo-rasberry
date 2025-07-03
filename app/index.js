b456391ca891d9b1419b8f880cf2d3e4fe8fb5ac
mathias-developpe <mathias.developpe@gmail.com>
2025-07-03 03:40:19 +0200

    Add version in api to test auto update

--- app/index.js
+++ app/index.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Rimoo Raspberry App! Main service is running.');
+  res.send('Hello from Rimoo Raspberry App! Main service is running. v1.0.0');
 });
 
 app.listen(port, () => {
   console.log(`Main app listening at http://localhost:${port}`);
 });
 
-console.log('Main application started.'); 
+console.log('Main application started.');
